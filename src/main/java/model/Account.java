39b01e87b2f2c08299b0eee5a65ece4d15382a41
EliasRademacher <rademace@onid.oregonstate.edu>
2016-11-23 21:36:18 -0800

    Add field userId to Account.

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -1,13 +1,35 @@
 package model;
 
 
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.users.User;
+
+import java.util.List;
+
 /**
  * Created by Elias on 11/23/2016.
  */
 public class Account {
-    /* Replace usages of model.Person with appengine.api.User,
+    private List<Key> challengesCompleted;
+
+    private List<Key> challengesPending;
+
+    /* TODO: Replace usages of model.Person with appengine.api.User,
     and migrate appropriate fields of model.Person to model.Account. */
 
     private Person fozoUser;
+
+    private String userId;
+
+    /* TODO: Change this to return an appengine.api.User. */
+    public Person getUser() {
+        return fozoUser;
+    }
+
+    /* TODO: Change this to set this.user */
+    public void setUser(Person user) {
+        this.fozoUser = user;
+    }
+
 
 }
