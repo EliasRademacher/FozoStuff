c8f14aa0ac40694d15b249dc1dacf845ff61b08c
EliasRademacher <rademace@onid.oregonstate.edu>
2016-11-23 21:23:40 -0800

    Create class "Account".

---
+++ src/main/java/model/Account.java
@@ -0,0 +1,13 @@
+package model;
+
+
+/**
+ * Created by Elias on 11/23/2016.
+ */
+public class Account {
+    /* Replace usages of model.Person with appengine.api.User,
+    and migrate appropriate fields of model.Person to model.Account. */
+
+    private Person fozoUser;
+
+}
